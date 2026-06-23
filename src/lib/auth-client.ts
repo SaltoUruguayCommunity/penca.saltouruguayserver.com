34e75da3807a4a8fd4afcf37a67ee45012994666
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-06-23 20:21:38 -0300

    add onetap

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,3 +9,17 @@ export const getSessionClient: () => Promise<Session | null> = async () => {
     return null;
   }
 };
+
+export function handleSignIn() {
+  const width = 580;
+  const height = 680;
+  const left = window.innerWidth / 2 - width / 2;
+  const top = window.innerHeight / 2 - height / 2;
+
+  window.open(
+    "/auth/sus",
+    "Iniciar sesión",
+    `toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,copyhistory=no,width=${width},height=${height},top=${top},left=${left}`,
+  );
+
+}
